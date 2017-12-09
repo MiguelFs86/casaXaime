6d04c0060cf4b9e22de9dbbbfcdaf22879f1a8e7
Miguel Fuentes <miguel.fuentes.sobrin@gmail.com>
2017-12-09 14:19:05 +0100

    <feature> Add new styles and images to "house" page

--- src/app/components/thehouse/thehouse.component.ts
+++ src/app/components/thehouse/thehouse.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare var jQuery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-thehouse',
@@ -25,9 +27,16 @@ export class ThehouseComponent implements OnInit {
     activities = [
       {'title': 'Senderismo', 'description':'Disfrute de una gran cantidad de rutas y caminos alrededor de una paisaje tan único y especial como As Fragas do Eume.', 'image':'./assets/img/activities/trekking.png'},
       {'title': 'Paseos a caballo', 'description':'Organiza tu ruta o paseo en caballo para contemplar las maravillas del paisaje.', 'image':'./assets/img/activities/horse_riding.png'},
-      {'title': 'Desdenso en canoa', 'description':'Descienda por el río Eume, explorando las Fragas desde dentro, montado en una canoa.', 'image':'./assets/img/activities/canoeing.png'},
+      {'title': 'Desdenso en canoa', 'description':'Descienda por el río Eume, explorando las Fragas desde dentro, montado en una canoa. <a target=_blank a href=www.cdfragasdoeume.com>www.cdfragasdoeume.com</a>', 'image':'./assets/img/activities/canoeing.png'},
       {'title': 'Rutas en Quad', 'description':'Disfrute de una ruta en Quad organizada para explorar el entorno natural.', 'image':'./assets/img/activities/quading.png'},
-      {'title': 'Turismo histórico', 'description':'En los alrededores de Casa Xaime existen un gran número de museos y turismo histórico, como el Monasterio de Monfero, el Monasterio de Caaveiro o la Torre de Andrade.', 'image':'./assets/img/activities/museum.png'}
+      {'title': 'Turismo histórico', 'description':'En los alrededores de Casa Xaime existen un gran número de museos y turismo histórico, como el Monasterio de Monfero, el Monasterio de Caaveiro o la Torre de Andrade.', 'image':'./assets/img/activities/museum.png'},
+      {'title': 'Rutas en Quad', 'description':'<a href="www.google.es">www.google.es</a>', 'image':'./assets/img/activities/quading.png'},
+    ]
+
+    imageArray = [
+      {'title':'Fragas do Eume', 'thumb':'./assets/img/activities/fragas_aerial_thumb.jpg','src':'./assets/img/activities/fragas_aerial.jpg'},
+      {'title':'Monasterio de Caaveiro', 'thumb':'./assets/img/activities/caaveiro_thumb.jpg','src':'./assets/img/activities/caaveiro.jpg'},
+      {'title':'Monasterio de Monfero', 'thumb':'./assets/img/activities/monfero_thumb.jpg','src':'./assets/img/activities/monfero.jpg'}
     ]
 
   ngOnInit() {
