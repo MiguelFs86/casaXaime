9ee255d114437849a5c4f8c9e62f2d7438701dff
Miguel Fuentes <miguel.fuentes.sobrin@gmail.com>
2017-12-11 14:47:59 +0100

    <feature> Add terms and conditions page, update translations and modify some texts.

---
+++ src/app/components/termsandconditions/termsandconditions.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-termsandconditions',
+  templateUrl: './termsandconditions.component.html'
+})
+export class TermsandconditionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
